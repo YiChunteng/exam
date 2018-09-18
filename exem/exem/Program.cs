46e71b094f36778213747be183880c9edc4429c6
YiChunteng <2017302580160@whu.edu.cn>
2018-09-18 08:50:47 +0800

    exam

---
+++ exem/exem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exem
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("实验1");
+		}
+	}
+}
